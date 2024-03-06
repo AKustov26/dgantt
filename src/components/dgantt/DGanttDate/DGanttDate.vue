085437e352777e22feb46da616ac63d9ecd5fb00
an_kustov <andreykustov26@gmail.com>
2024-03-06 22:26:17 +0700

    add component DGanttDate

---
+++ src/components/dgantt/DGanttDate/DGanttDate.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="dgantt-date">
+		<div class="scale"></div>
+		<div class="scale"></div>
+	</div>
+</template>
+
+<script setup lang="ts">
+// Изменение даты (месяца, дни, часы) - приходит в props
+// 
+// eachDayOfInterval -> date fns
+</script>
