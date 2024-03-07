e9a0ea12cf46904808709c646fc93358b0770ebb
an_kustov <andreykustov26@gmail.com>
2024-03-07 11:28:40 +0700

    delete default project file

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueApexCharts from "vue3-apexcharts";
 
-const app = createApp(App);
-app.use(VueApexCharts);
 createApp(App).mount('#app')
